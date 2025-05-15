a25a3c27733936f5c9adef37dc780093feb1fff8
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-05-15 13:13:29 +0200

    Tailwind css + daisyUI Installation

---
+++ src/app/anlegen/anlegen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anlegen',
+  imports: [],
+  templateUrl: './anlegen.component.html',
+  styleUrl: './anlegen.component.css'
+})
+export class AnlegenComponent {
+
+}
