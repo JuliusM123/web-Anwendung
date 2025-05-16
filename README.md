45e89e60964908764628809b9ecaee1c31d57d37
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-05-16 10:27:11 +0200

    fix: Namensänderung

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WegAnwendung
+# WebAnwendung
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.11.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'weg-Anwendung' title`, () => {
+  it(`should have the 'web-Anwendung' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weg-Anwendung');
+    expect(app.title).toEqual('web-Anwendung');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, weg-Anwendung');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, web-Anwendung');
   });
 });
