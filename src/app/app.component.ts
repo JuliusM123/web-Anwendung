abca85ab3748be1f2ec797b124181a9252cc04c6
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-05-16 10:24:01 +0200

    feat: Login Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { SucheComponent } from "./suche/suche.component";
 import { AnlegenComponent } from "./anlegen/anlegen.component";
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SucheComponent, AnlegenComponent, RouterOutlet, RouterLink],
+  imports: [SucheComponent, AnlegenComponent, LoginComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SucheComponent } from './suche/suche.component';
 import { AnlegenComponent } from './anlegen/anlegen.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: 'suchen', component: SucheComponent},
     {path: 'anlegen', component: AnlegenComponent},
+    {path: 'login', component: LoginComponent},
 ];
