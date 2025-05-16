7f36e482e54bdb88812b3d134908e1424d6346d3
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-05-16 11:08:23 +0200

    fix: esLint Fehler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { SucheComponent } from './suche/suche.component';
-import { AnlegenComponent } from './anlegen/anlegen.component';
-import { LoginComponent } from './login/login.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
-        SucheComponent,
-        AnlegenComponent,
-        LoginComponent,
         RouterOutlet,
         RouterLink,
     ],
