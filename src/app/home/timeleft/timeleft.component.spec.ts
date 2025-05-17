0756e71c5a979bbc93a08fa10ece4c2ffc08a335
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-05-17 09:42:37 +0200

    fix: Prettier

--- src/app/home/timeleft/timeleft.component.spec.ts
+++ src/app/home/timeleft/timeleft.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TimeleftComponent } from './timeleft.component';
 
 describe('TimeleftComponent', () => {
-  let component: TimeleftComponent;
-  let fixture: ComponentFixture<TimeleftComponent>;
+    let component: TimeleftComponent;
+    let fixture: ComponentFixture<TimeleftComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TimeleftComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TimeleftComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(TimeleftComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TimeleftComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
