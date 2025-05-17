5b2faf7b0fb152751b98f76f3203ab05c1ee3208
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-05-17 08:15:00 +0200

    fix: Hintergrundseite für Login

--- src/app/suche/suche.component.ts
+++ src/app/suche/suche.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-suche',
     standalone: true,
-    imports: [],
     templateUrl: './suche.component.html',
     styleUrl: './suche.component.css',
 })
