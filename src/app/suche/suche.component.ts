1bfcb3cf5a2172fc5a4e77eef7a86443879df790
Philipp3857 <148771095+Philipp3857@users.noreply.github.com>
2025-05-22 11:49:01 +0200

    → feat(suche): Suchfunktion und HTTP-Request hinzugefügt

--- src/app/suche/suche.component.ts
+++ src/app/suche/suche.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Buch } from './buch.model';
+import { firstValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
     selector: 'app-suche',
     standalone: true,
+    imports: [CommonModule, FormsModule],
     templateUrl: './suche.component.html',
-    styleUrl: './suche.component.css',
+    styleUrls: ['./suche.component.css'],
 })
-export class SucheComponent {}
+
+export class SucheComponent {
+
+    private http = inject(HttpClient);
+    public buch: Buch | null | undefined;
+    titel = '';
+    isbn = '';
+    rating = '';
+    buchart = '';
+    lieferbar: boolean | ''= false;
+
+async suchen(titel: string, isbn: string, rating: string, buchart: string, lieferbar: boolean | '') {
+  console.log('Suchen wurde aufgerufen');
+  const params: any = {};
+  if (isbn) params.isbn = isbn;
+  if (titel) params.titel = titel;
+  if (rating) params.rating = rating;
+  if (buchart) params.art = buchart;
+  if (lieferbar !== '') params.lieferbar = lieferbar;
+
+  try {
+    const response: any = await firstValueFrom(
+      this.http.get('https://localhost:3000/rest', { params })
+    );
+    console.log('Antwort:', response);
+
+    this.buch = response.content?.[0] ?? null;
+  } catch (err) {
+    console.error('Fehler beim Suchen', err);
+  }
+}
+}
