4270d9c2cae4d9797f0a6c0e96f3d2016b9c9520
Julius Moeckel <147931716+JuliusM123@users.noreply.github.com>
2025-06-01 10:42:38 +0200

    feat: Error Message anzeigen

--- src/app/anlegen/anlegen.component.ts
+++ src/app/anlegen/anlegen.component.ts
@@ -6,8 +6,8 @@ import {
 } from '@angular/common/http';
 import { Buch, BuchArt } from '../../types/buch.model';
 import Decimal from 'decimal.js';
-import { firstValueFrom } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-anlegen',
@@ -26,9 +26,10 @@ export class AnlegenComponent {
     homepage = '';
     art: BuchArt | '' = '';
     lieferbar = true;
+    erroer: HttpErrorResponse | null = null;
 
     readonly #token =
-        'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJDZ1NOdmJRTjRvVGxCZktEbEM1OGZMcFA5NF9JNEFVYlhueWxydkFGQ1lvIn0.eyJleHAiOjE3NDg2ODg1NjYsImlhdCI6MTc0ODY4ODI2NiwianRpIjoib25ydHJvOmY3NmQ3MDM4LTk5NzItNDgyMC05MTUzLWVkZDMwNzNjYWI3OCIsImlzcyI6Imh0dHA6Ly9rZXljbG9hazo4MDgwL3JlYWxtcy9uZXN0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImM1MTY2OGRhLTBiYTUtNDdlMS05YTljLTQzZGFmMzdhZmQyNSIsInR5cCI6IkJlYXJlciIsImF6cCI6Im5lc3QtY2xpZW50Iiwic2lkIjoiNzdmMTE1ZWQtYzhlZS00OGZiLTk0MzItMjRiMzM3NTQxNTZjIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2xvY2FsaG9zdDozMDAwIiwiaHR0cHM6Ly9vYXV0aC5wc3Rtbi5pbyIsImh0dHBzOi8vYXV0bzozMDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1uZXN0Il19LCJyZXNvdXJjZV9hY2Nlc3MiOnsibmVzdC1jbGllbnQiOnsicm9sZXMiOlsiYWRtaW4iXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6Ik5lc3QgQWRtaW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhZG1pbiIsImdpdmVuX25hbWUiOiJOZXN0IiwiZmFtaWx5X25hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWNtZS5jb20ifQ.XX5VApaCBGe0rUC7pI7dslhk-NxrMzxtjSgdvvCaGNxGoqiRKPT-2hNhegceGbk83wXgUjfAzCMQn54KwDyvEfkmGPaU9lWl0KYsY_fni7aQ7sertxgA6FeQDFCGG3kdf017Hmk3nwO2DiSV9317gn04b9qNsHNGJNnHvzFgB6BmkGZi8H7ZV9NJehVvgpHORtgqApGdJ15eav4oBEXD6xLSXmP7-bngSCMXAT3V2d-lcZjZIq8UccvPKA0pNkdnlWfn6U0zI7l5l9CaZ7X6cjHeHKA_k3Q9mC-iD0HMmxnlOEAllHmhJOJ8XNf38JLUQmEXc8dcgPFJXK7BUAS4Cw'; // bis Login steht manuell setzen
+        'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJDZ1NOdmJRTjRvVGxCZktEbEM1OGZMcFA5NF9JNEFVYlhueWxydkFGQ1lvIn0.eyJleHAiOjE3NDg3NjYzOTUsImlhdCI6MTc0ODc2NjA5NSwianRpIjoib25ydHJvOjM1MmQ2YTU4LWE5Y2EtNGUwNS1iOTA4LTAyZmYyYmQwMDUzYSIsImlzcyI6Imh0dHA6Ly9rZXljbG9hazo4MDgwL3JlYWxtcy9uZXN0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImM1MTY2OGRhLTBiYTUtNDdlMS05YTljLTQzZGFmMzdhZmQyNSIsInR5cCI6IkJlYXJlciIsImF6cCI6Im5lc3QtY2xpZW50Iiwic2lkIjoiODk2MDE4NmUtYWI4Ny00NDRlLWEyZDktMjI0YTdlYjBhNDc3IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2xvY2FsaG9zdDozMDAwIiwiaHR0cHM6Ly9vYXV0aC5wc3Rtbi5pbyIsImh0dHBzOi8vYXV0bzozMDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1uZXN0Il19LCJyZXNvdXJjZV9hY2Nlc3MiOnsibmVzdC1jbGllbnQiOnsicm9sZXMiOlsiYWRtaW4iXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6Ik5lc3QgQWRtaW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhZG1pbiIsImdpdmVuX25hbWUiOiJOZXN0IiwiZmFtaWx5X25hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWNtZS5jb20ifQ.zhYyUws0rvr-jcsKVWHZy-t0m5OulkTQ9urO1G-bBW8voGpJxN6mo9CrzgxsV8mMfmYG0PDwH3Y28huN4duH7yH0GyGKmsKOA0zzWaBDCrzj16hOP9f-yLuBEjxaRQM7AZSt-aPiY8k78IeKqmdyp2eIV3eVUdsCiwSZMJKnm54vW0LeiAPSP4mKMi1BYXtZVtYOV84LJhWevhX1rcBzHL95bt2nG3u1iF17GYfTQPxOsdrnMoMtGpnSCKDk-Viabn-d31E6BlxqnwDBLOXN9vFmhBOfSVff57wj4ZZ5MaYtPUU7kwgaX_IouRA64lNeOQfW0HZwiVnT3dttU-h52w'; // bis Login steht manuell setzen
 
     async buchSenden() {
         const dummyBuch: Buch = {
@@ -58,17 +59,14 @@ export class AnlegenComponent {
         });
         try {
             const result = await firstValueFrom(
-                this.#http.post<Buch>(
-                    'https://localhost:3000/rest',
-                    dummyBuch,
-                    {
-                        headers,
-                    },
-                ),
+                this.#http.post('https://localhost:3000/rest', dummyBuch, {
+                    headers,
+                }),
             );
             console.log('Buch erfolgreich angelegt:', result);
         } catch (err) {
             if (err instanceof HttpErrorResponse) {
+                this.erroer = err;
                 console.error('Fehler beim Anlegen:', err.status, err.message);
             } else {
                 console.error('Unbekannter Fehler:', err);
