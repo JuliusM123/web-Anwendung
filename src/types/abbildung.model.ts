b3e5cab2f0035b2a5e5fa0e9bcebd3a004e1d05a
davidjjflaig <davidflaig52@gmail.com>
2025-06-03 19:41:57 +0200

    refactor: small changes on abbildung.ts

--- src/types/abbildung.model.ts
+++ src/types/abbildung.model.ts
@@ -1,3 +1,9 @@
+/**
+ * @interface Abbildung
+ * @description
+ * Definiert die Struktur für ein Abbildungsobjekt.
+ * Eine Abbildung kann eine ID, eine Beschriftung und einen Content-Typ haben.
+ */
 export interface Abbildung {
     id?: number;
     beschriftung?: string;
