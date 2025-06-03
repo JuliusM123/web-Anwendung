99ba211395096f6b28b49f754ac415f118975667
davidjjflaig <davidflaig52@gmail.com>
2025-06-03 19:44:38 +0200

    refactor: small changes on titel.ts

--- src/types/titel.model.ts
+++ src/types/titel.model.ts
@@ -1,3 +1,9 @@
+/**
+ * @interface Titel
+ * @description
+ * Definiert die Struktur für den Titel eines Buches.
+ * Ein Titel besteht aus einem Haupttitel und optional einem Untertitel.
+ */
 export interface Titel {
     titel: string;
     untertitel?: string;
