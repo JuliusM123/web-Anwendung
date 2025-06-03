378270ef08f118073d0fa36edbacb4f744e25409
Jonas Kirschner <kirschner.jonas@gmx.de>
2025-06-03 19:35:30 +0200

    refactor:tsdoc changes for app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+/**
+ * @constant appConfig
+ * @description
+ * Defines the application's root configuration for standalone Angular applications.
+ * It sets up essential providers for routing, HTTP client functionality,
+ * and zone change detection.
+ * @type {ApplicationConfig}
+ */
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideHttpClient(withFetch()),
-    ],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), // Configures zone.js change detection
+    provideRouter(routes), // Provides the application routes
+    provideHttpClient(withFetch()), // Configures HttpClient to use the fetch API
+  ],
 };
