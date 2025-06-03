8bdffa1fc31fdccf477bf71bf79d197a71ca384e
Jonas Kirschner <kirschner.jonas@gmx.de>
2025-06-03 19:36:16 +0200

    refactor: small changes in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
+/**
+ * @Component AppComponent
+ * @description
+ * The root component of the application.
+ * It serves as the main entry point and container for other components and views.
+ * Includes RouterOutlet for displaying routed components and RouterLink for navigation.
+ */
 @Component({
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet, RouterLink],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-    title = 'web-Anwendung';
+  title = 'web-Anwendung';
 }
