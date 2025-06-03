0678873e9aff9dda8a6ff804c584b3f09bbdd94f
davidjjflaig <davidflaig52@gmail.com>
2025-06-03 19:43:43 +0200

    refactor: small changes on buch.ts

--- src/types/buch.model.ts
+++ src/types/buch.model.ts
@@ -2,18 +2,30 @@ import type Decimal from 'decimal.js';
 import type { Abbildung } from './abbildung.model';
 import type { Titel } from './titel.model';
 
+/**
+ * @type {BuchArt}
+ * @description
+ * Definiert die möglichen Arten eines Buches.
+ * Kann 'EPUB', 'HARDCOVER' oder 'PAPERBACK' sein.
+ */
 export type BuchArt = 'EPUB' | 'HARDCOVER' | 'PAPERBACK';
 
+/**
+ * @interface Buch
+ * @description
+ * Definiert die Struktur für ein Buchobjekt.
+ * Enthält alle relevanten Informationen zu einem Buch, wie ISBN, Bewertung, Art, Preis, etc.
+ */
 export interface Buch {
-    isbn: string;
-    rating: number;
-    art: BuchArt;
-    preis: Decimal;
-    rabatt: Decimal;
-    lieferbar: boolean;
-    datum: string;
-    homepage: string;
-    schlagwoerter: string[];
-    titel: Titel;
-    abbildung: Abbildung[];
+  isbn: string;
+  rating: number;
+  art: BuchArt;
+  preis: Decimal;
+  rabatt: Decimal;
+  lieferbar: boolean;
+  datum: string;
+  homepage: string;
+  schlagwoerter: string[];
+  titel: Titel;
+  abbildung: Abbildung[];
 }
