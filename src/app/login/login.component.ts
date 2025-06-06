7bd0fc8b34daefb746bf0b67890deb8b1fd498ab
JBones35 <kirschner.jonas@gmx.de>
2025-06-06 18:49:58 +0200

    feat: hinzufügen von logging und verändern der html datei

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'app-login',
     standalone: true,
+    imports: [
+        FormsModule
+    ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
 })
@@ -13,5 +17,12 @@ export class LoginComponent {
 
     username = '';
     password = '';
+
+    login() {
+        console.log('Login attempt with:', {
+            username: this.username,
+        });
+        
+    }
     
 }
