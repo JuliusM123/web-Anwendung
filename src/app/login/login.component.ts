372dab4ecd37a5ca19717895e2b9dc3f4c57a272
JBones35 <kirschner.jonas@gmx.de>
2025-06-06 19:22:00 +0200

    feat: modify async function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom';
+
+const url = 'https://localhost:3000/auth/token';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,12 +21,36 @@ export class LoginComponent {
 
     username = '';
     password = '';
+    error: HttpErrorResponse | null = null;
+    responseStatus: number | null = null;
 
-    login() {
-        console.log('Login attempt with:', {
-            username: this.username,
-        });
-        
+async login() {
+    const loginData = {
+        username: this.username,
+        password: this.password
+    };
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+    });
+
+    try {
+        const response = await firstValueFrom(
+            this.#httpClient.post(url, loginData, { observe: 'response', headers })
+        );
+
+        this.responseStatus = response.status;
+        this.error = null;
+        console.log('Login successful:', response);
+
+    } catch (err) {
+        if (err instanceof HttpErrorResponse) {
+            this.responseStatus = err.status;
+            this.error = err;
+            console.error('Login failed:', err);
+        } else {
+            console.error('An unexpected error occurred:', err);
+        }
     }
+}
     
 }
