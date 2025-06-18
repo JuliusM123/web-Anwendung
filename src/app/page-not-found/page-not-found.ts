563b13cc6238480829f890a15e49c9442bb250ab
JBones35 <kirschner.jonas@gmx.de>
2025-06-18 10:12:39 +0200

    feat:tsdoc für notFound.ts

--- src/app/page-not-found/page-not-found.ts
+++ src/app/page-not-found/page-not-found.ts
@@ -1,10 +1,16 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+/**
+ * Die `PageNotFoundComponent` ist eine einfache Komponente, die angezeigt wird,
+ * wenn ein Benutzer versucht, auf eine nicht existierende Route zuzugreifen.
+ * Sie nutzt die `OnPush` Change Detection Strategy für eine optimierte Leistung.
+ */
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'app-page-not-found',
     standalone: true,
     imports: [],
     templateUrl: './page-not-found.html',
-    styleUrl: './page-not-found.css',
+    styleUrls: ['./page-not-found.css'],
 })
 export class PageNotFoundComponent {}
