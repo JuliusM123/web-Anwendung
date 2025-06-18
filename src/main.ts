b907b906bf71590ce585ff12fa13b840d3e21a3b
JBones35 <kirschner.jonas@gmx.de>
2025-06-18 10:16:18 +0200

    feat:tsdoc für main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
+/**
+ * Startet die Angular-Anwendung.
+ *
+ * Diese Datei ist der Einstiegspunkt der Anwendung. Sie bootstrappt die
+ * {@link AppComponent} und verwendet die in {@link appConfig} definierten
+ * Konfigurationen.
+ *
+ * Wenn beim Starten der Anwendung ein Fehler auftritt, wird dieser in der Konsole ausgegeben.
+ */
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err),
 );
