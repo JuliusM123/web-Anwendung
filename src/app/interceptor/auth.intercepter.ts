5cb8f052824d343fc628c13233c3804662e000f5
JBones35 <kirschner.jonas@gmx.de>
2025-06-18 07:59:11 +0200

    feat:tsdoc für auth.inteceptor.ts

--- src/app/interceptor/auth.intercepter.ts
+++ src/app/interceptor/auth.intercepter.ts
@@ -2,9 +2,20 @@ import type { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 
+/**
+ * Ein HTTP-Interceptor, der den `Authorization`-Header zu ausgehenden Anfragen hinzufügt.
+ *
+ * Dieser Interceptor hat folgende Logik:
+ * 1. Ruft den Authentifizierungsdienst auf, um den aktuellen Token zu erhalten.
+ * 2. Wenn ein Token vorhanden ist UND die Anforderungs-URL NICHT den Pfad `/auth/` enthält
+ * (um zu vermeiden, dass Authentifizierungs-Endpunkte selbst mit einem Token versehen werden),
+ * wird der `Authorization`-Header mit dem Format `Bearer [Token]` gesetzt.
+ * 3. Die modifizierte oder ursprüngliche Anfrage wird an den nächsten Handler in der Kette weitergeleitet.
+ */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService);
     const authToken = authService.getToken();
+
     if (authToken && !req.url.includes('/auth/')) {
         const authReq = req.clone({
             setHeaders: {
